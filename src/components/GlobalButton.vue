b12fb8d37873b689e14ecb8ecff9cc9e9581b775
TankManBeta <1148099884@qq.com>
2025-08-22 17:47:25 +0800

    Add some usage demo of Vue3.

---
+++ src/components/GlobalButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <button @click="handleClick" class="btn">{{ label }}</button>
+</template>
+
+<script>
+export default {
+  props: ["label"],
+  methods: {
+    handleClick() {
+      alert("全局按钮被点击了！")
+    }
+  }
+}
+</script>
+
+<style>
+.btn {
+  padding: 10px;
+  background: #42b983;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+</style>
---
+++ src/components/LocalButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <button @click="handleClick" class="btn">{{ label }}</button>
+</template>
+
+<script>
+export default {
+  props: ["label"],
+  methods: {
+    handleClick() {
+      alert("局部按钮被点击了！")
+    }
+  }
+}
+</script>
